b810423db09593f6b037f621489d4664452d3756
Aroxed <petrashenko@gmail.com>
2024-04-17 14:58:40 +0300

    files order

--- 5_coroutine_progress_of_work.py
+++ 5_coroutine_progress_of_work.py
@@ -1,10 +1,12 @@
 import asyncio
 import random
 
+
 # Coroutine to simulate work
 async def work(id):
     await asyncio.sleep(random.uniform(0.5, 2.5))
     print(f"Task {id} finished")
+
 
 # Coroutine to display progress
 async def display_progress():
@@ -12,11 +14,13 @@ async def display_progress():
         print(f"Progress: {i * 20}%")
         await asyncio.sleep(1)
 
+
 # Main coroutine
 async def main():
     tasks = [asyncio.create_task(work(i)) for i in range(1, 6)]
-    asyncio.create_task(display_progress())
+    await asyncio.create_task(display_progress())
     await asyncio.gather(*tasks)
+
 
 # Run the main coroutine
 asyncio.run(main())
